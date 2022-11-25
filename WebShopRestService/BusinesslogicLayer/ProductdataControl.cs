c1e09140ddfbfb116f28f6e7bfdc092235a6db83
Bon <masterpiyaboot@hotmail.com>
2022-11-25 09:16:00 +0100

    Initial commit

---
+++ WebShopRestService/BusinesslogicLayer/ProductdataControl.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebShopData.DatabaseLayer;
+using WebShopModel.Model;
+
+namespace WebShopService.BusinesslogicLayer
+{
+    public class ProductdataControl : IProductdata
+    {
+        IProductAccess _productAccess;
+        
+        public ProductdataControl(IConfiguration inConfiguration)
+        {
+            _productAccess = new ProductDatabaseAccess(inConfiguration);
+        }
+        
+        public List<Product> Get()
+        {
+            List<Product>? foundProducts;
+            try
+            {
+                foundProducts = _productAccess.GetProductAll();
+            }
+            catch
+            {
+                foundProducts = null;
+            }
+            return foundProducts;
+        }
+
+    }
+}
